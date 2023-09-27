a76d504216dbc29a6cbd3877b97854c3f97868a0
KRossCTS <kross@cooktopsafety.com>
2023-09-27 15:28:42 -0400

    Added Canny edge detector and stacked image

--- johns_calibration.py
+++ johns_calibration.py
@@ -1,25 +1,83 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 19 08:50:08 2023
-
-@author: Kyle Ross
-
 Revised script for calibrating data
 Does not contain the process for averaging the bounding boxes
 
 Input is thermal data from a .cts file
 """
 
+# pylint: disable=E1101
 
 import numpy as np
 import os
-import cv2 as cv
-
-from skimage import filters
+import cv2
 
 import dataset_functions as df
-
 import numpy as np
+
+
+## Define parameters
+
+# Define the radius and sigma for the dissipating circle kernel
+radius = 4
+sigma = .5
+
+# Threshold values
+temp_threshold = 50
+normalization_threshold = .4
+
+# Convolution iterations
+iterations = 2
+
+
+def stackImages(scale, imgArray, captions=None):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                # Resize the image
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                
+                # Add captions to the images
+                cv2.putText(imgArray[x][y], captions[x][y], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                
+                # Convert grayscale images to BGR
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            # Resize the image
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            
+            # Add captions to the images
+            cv2.putText(imgArray[x], captions[x], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            
+            # Convert grayscale images to BGR
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        
+        hor = np.hstack(imgArray)
+        ver = hor
+    
+    return ver
 
 
 def create_dissipating_circle_kernel(radius, sigma):
@@ -48,27 +106,12 @@ def create_dissipating_circle_kernel(radius, sigma):
     return kernel
 
 
-def otsu(roi, classes):
-    # Mask 0s to be empty cells to allow better otsu thresholding without 0 being a class
-    while(classes > 0):
-        try:
-            thresholds = filters.threshold_multiotsu(roi, classes)
-            break
-        except:
-            classes = classes-1
-
-    otsu = np.digitize(roi, bins=thresholds)
-    return otsu, thresholds
-
-
 def get_thermal(directory, file):
     thermal_file = os.path.join(directory, file)
     thermal_data = np.load(thermal_file)
     # thermal_time = thermal_data['time']
     return thermal_data
 
-
-files = []
 
 # Get all the files for which the calibration doesn't work correctly
 summer_data = 'Documents/finalDataRepositorySummer2022'
@@ -79,129 +122,79 @@ non_calibrated_files = list(
 non_calibrated_files = ['JohnCalibration.npy']
 
 
-# Example usage:
-radius = 4  # Adjust the radius as needed
-sigma = .5  # Adjust the sigma (spread) as needed
 kernel = create_dissipating_circle_kernel(radius, sigma)
 
-# Print or visualize the kernel
-print(kernel)
+
+def create_mask(normalization_threshold, kernel, frame, iterations=1):
+    '''
+            Perform convolution using the dissipating heatmap (which mimics a burner), then normalize.
+            Each iteration of the convolution will increase the intensity of the burner regions
+            and suppress the other regions. 
+            After doing this a few times, the burner regions will be the only ones with a high intensity, hopefully.
+
+            Args:
+                normalization_threshold (float): The threshold for the normalized convolution result.
+                kernel (numpy.ndarray): The kernel to use for the convolution.
+                frame (numpy.ndarray): The frame to convolve.
+                iterations (int): The number of iterations to perform.
+    '''
+
+    colvolved_frame = frame.copy()
+    for i in range(iterations):
+        colvolved_frame = cv2.filter2D(colvolved_frame, -1, kernel)
+        min_value = abs(np.min(colvolved_frame))
+        max_value = abs(np.max(colvolved_frame))
+        colvolved_frame = (colvolved_frame - min_value) / (max_value - min_value) # Normalize the convolution result
+        colvolved_frame[colvolved_frame < normalization_threshold] = 0
+
+    return colvolved_frame
 
 
-# Perform Connected Component Analysis
-def connected_component_analysis(binary_image):
-    num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(
-        binary_image, connectivity=8)
-
-    # num_labels: Total number of connected components (including background)
-    # labels: Label image where each pixel is assigned a label (0 for background)
-    # stats: Statistics for each connected component (e.g., left, top, width, height)
-    # centroids: Centroid coordinates (x, y) for each connected component
-
-    # Create an output image to visualize the labeled components
-    output_image = np.zeros_like(binary_image)
-
-    # Loop through connected components (excluding background)
-    for label in range(1, num_labels):
-        # Extract statistics for the current component
-        left, top, width, height, area = stats[label]
-
-    # Draw a bounding box around the component on the output image
-        cv.rectangle(output_image, (left, top),
-                     (left + width, top + height), (255, 255, 255), 2)
-
-    # Display or save the output image with bounding boxes
-    cv.imshow('Connected Components', cv.resize(
-        output_image.astype('uint8'), (320, 320)))
-    cv.waitKey(2000)
-
-
-prev_frame = []
-diffs = []
-skip = 0
 for file in non_calibrated_files:
     file_dir = os.path.join(data_dir, file)
-    print("file _----------------------------")
-    print(file_dir)
 
     thermal = None
     thermal = get_thermal(data_dir, file)
-
     thermal = np.array(df.resample_data(thermal))
-    print(thermal.shape)
+    print(thermal.shape) #check how many frames the thermal image has
 
     if thermal is not None:
-        for frame in thermal:
-            key = cv.waitKey(1000)
-            print(frame.shape)
-            cv.imshow("Thermal Original", cv.resize(
-                frame.astype('uint8'), (320, 320)))
+        for i, frame in enumerate(thermal):
+            key = cv2.waitKey(1000)
+            print(f"Frame: {frame.shape}")
 
-            detection = np.copy(frame)
-            detection[frame < 50] = 0
-            # Apply adaptive thresholding
-            # detection = cv.cvtColor(detection, cv.COLOR_BGR2GRAY)
-            print(detection.shape)
+            original_frame = cv2.resize(np.copy(frame).astype('uint8'), (320, 320))
+            frame[frame < temp_threshold] = 0 #All regions colder than the threshold should be black in the image
+            # detection = cv2.cv2tColor(detection, cv2.COLOR_BGR2GRAY)
 
-            # Apply convolution with the dissipating circle kernel
-            result = cv.filter2D(detection, -1, kernel)
+            # Apply convolution with the dissipating circle kernel to create a mask
+            colvolved_frame = create_mask(normalization_threshold, kernel, frame, iterations=iterations)
+            frame = np.multiply(colvolved_frame, 255) # Apply the mask to the original image
+            frame[frame < temp_threshold] = 0 #All regions colder than the threshold should be black in the image
 
-            # Define a threshold to identify potential burners
-            threshold = 10000  # Adjust as needed
-            # Find the minimum value in the result image
+            #dilate the edges
+            frame = frame.astype(np.uint8)
+            frame = cv2.Canny(frame, 100, 180)
+            dilation_kernel = np.ones((1, 1), np.uint8)
+            frame = cv2.dilate(frame, dilation_kernel, iterations=iterations)
 
-            '''
-            Perform convolution using the dissipating heatmap (which mimics a burner)
-            then normalize
-            Each iteration of the convolution will increase the intensity of the burner regions
-            and suppress the other regions. 
-            After doing this a few times, the burner regions will be the only ones with a high intensity, hopefully
+            processed_frame = cv2.resize(np.copy(frame).astype('uint8'), (320, 320))
 
-            '''
-            min_value = abs(np.min(result))
-            max_value = abs(np.max(result))
-
-            # Normalize the result image by dividing by the minimum value
-            result = (result - min_value) / (max_value - min_value)
-            result[result < .4] = 0
-            result = cv.filter2D(result, -1, kernel)
-
-            min_value = abs(np.min(result))
-            max_value = abs(np.max(result))
-            result = (result - min_value) / (max_value - min_value)
-
-            result[result < .4] = 0
-            result = cv.filter2D(result, -1, kernel)
-            min_value = abs(np.min(result))
-            max_value = abs(np.max(result))
-            result = (result - min_value) / (max_value - min_value)
-
-            # Apply the mask to the original image
-            frame = np.multiply(result, 255)
-            frame[frame < 50] = 0
-            cv.imshow("Normalized Result", cv.resize(
-                frame.astype('uint8'), (320, 320)))
-
-            # connected_component_analysis(frame.astype(np.uint8))
-
-            contours, _ = cv.findContours(frame.astype(
-                np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-
+            contours, _ = cv2.findContours(frame.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
             for _, c in enumerate(contours):
-                # Place a bounding box around the contour
-                x, y, w, h = cv.boundingRect(c)
-                print(x, y, w, h)
+                x, y, w, h = cv2.boundingRect(c)
 
                 # Check if the bounding box is an appropriate size for a burner
                 # if w>=3 and h>=3 and w<=12 and h<=12:
-                cv.rectangle(frame, (x, y), (x+w, y+h), (128, 128, 55), 1)
-                # coords.append([x,y,w,h])
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (128, 128, 55), 1)
 
-            cv.imshow("Detection", cv.resize(
-                frame.astype('uint8'), (320, 320)))
+            images_to_show = [original_frame, processed_frame, cv2.resize(frame.astype('uint8'), (320, 320))]
+            captions = [f"Original Frame: {i}", 'Processed Frame', 'Bounding Boxes']
+            imgStack = stackImages(1, [images_to_show], [captions])
+            cv2.imshow("Visualize Frames",imgStack)
 
             if key == 27:
                 break
 
 
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
