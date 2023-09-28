f0f972a4b0c3428aadef0568de77943ab84701a9
KRossCTS <kross@cooktopsafety.com>
2023-09-28 13:49:37 -0400

    working version of the script, increasing weight average, thermal image reference middle two third, might try Kyle's averaging next

--- johns_calibration.py
+++ johns_calibration.py
@@ -29,7 +29,6 @@ normalization_threshold = .4
 # Convolution iterations
 iterations = 2
 
-
 def stackImages(scale, imgArray, captions=None):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -119,7 +118,7 @@ summer_data = 'Documents/finalDataRepositorySummer2022'
 data_dir = os.path.join(os.path.expanduser('~'), summer_data)
 non_calibrated_files = list(
     np.load(os.path.join(data_dir, 'completedCalibration.npy')))
-non_calibrated_files = ['JohnCalibration.npy']
+# non_calibrated_files = ['JohnCalibration.npy'] #this line is for testing on John's burner data
 
 
 kernel = create_dissipating_circle_kernel(radius, sigma)
@@ -150,23 +149,67 @@ def create_mask(normalization_threshold, kernel, frame, iterations=1):
     return colvolved_frame
 
 
+def calculate_weighted_average_bbox(total_x, total_y, total_w, total_h, total_weight):
+    for i in range(len(total_x)):
+        total_x[i] = int(total_x[i] / total_weight)
+        total_y[i] = int(total_y[i] / total_weight)
+        total_w[i] = int(total_w[i] / total_weight)
+        total_h[i] = int(total_h[i] / total_weight)
+
+    return total_x, total_y, total_w, total_h
+
+
+def update_cumulative_values(ind_contour, x, y, w, h, weight, total_x, total_y, total_w, total_h):
+    if len(total_x)<ind_contour+1:
+        total_x.append(x * weight)
+        total_y.append(y * weight)
+        total_w.append(w * weight)
+        total_h.append(h * weight)
+    else:
+        total_x[ind_contour] += x * weight
+        total_y[ind_contour] += y * weight
+        total_w[ind_contour] += w * weight
+        total_h[ind_contour] += h * weight
+
+    return total_x, total_y, total_w, total_h
+
+
+def get_reference_frame(thermal):
+    # Determine the range of frames you want to include in the average
+    start_index = int(len(thermal) / 3)  # Start at 1/3 of the frames
+    end_index = int(2 * len(thermal) / 3)  # End at 2/3 of the frames
+
+    # Extract the frames in the specified range
+    frames_to_average = thermal[start_index:end_index]
+
+    # Calculate the average frame using NumPy
+    reference_frame = np.mean(frames_to_average, axis=0)
+    return reference_frame
+
+
 for file in non_calibrated_files:
     file_dir = os.path.join(data_dir, file)
 
     thermal = None
-    thermal = get_thermal(data_dir, file)
+    thermal = df.get_thermal(data_dir, file)
     thermal = np.array(df.resample_data(thermal))
     print(thermal.shape) #check how many frames the thermal image has
 
     if thermal is not None:
+        print(f"Processing {file}")
+
+        total_x, total_y, total_w, total_h, total_weight = [], [], [], [], 0
+        #this line generated increasing weights to make sure the most recent frames are weighted more heavily
+        frame_weights = np.linspace(.1, 1, len(thermal))
+
         for i, frame in enumerate(thermal):
-            key = cv2.waitKey(1000)
-            print(f"Frame: {frame.shape}")
-
+            key = cv2.waitKey(1)
+            # print(f"Frame: {frame.shape}")
+            # weight = frame_weights[i]
+            weight = frame_weights[i]
             original_frame = cv2.resize(np.copy(frame).astype('uint8'), (320, 320))
-            frame[frame < temp_threshold] = 0 #All regions colder than the threshold should be black in the image
-            # detection = cv2.cv2tColor(detection, cv2.COLOR_BGR2GRAY)
 
+            frame[frame < temp_threshold] = 0 #All regions colder than the threshold should be black in the image
             # Apply convolution with the dissipating circle kernel to create a mask
             colvolved_frame = create_mask(normalization_threshold, kernel, frame, iterations=iterations)
             frame = np.multiply(colvolved_frame, 255) # Apply the mask to the original image
@@ -181,12 +224,17 @@ for file in non_calibrated_files:
             processed_frame = cv2.resize(np.copy(frame).astype('uint8'), (320, 320))
 
             contours, _ = cv2.findContours(frame.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            for _, c in enumerate(contours):
+            for ind_contour, c in enumerate(contours):
                 x, y, w, h = cv2.boundingRect(c)
 
                 # Check if the bounding box is an appropriate size for a burner
-                # if w>=3 and h>=3 and w<=12 and h<=12:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (128, 128, 55), 1)
+                if w>=3 and h>=3 and w<=12 and h<=12:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (128, 128, 55), 1)
+                    total_x, total_y, total_w, total_h = update_cumulative_values(ind_contour,
+                                        x, y, w, h, weight, total_x, total_y, total_w, total_h)
+                    
+            total_weight += weight
+                
 
             images_to_show = [original_frame, processed_frame, cv2.resize(frame.astype('uint8'), (320, 320))]
             captions = [f"Original Frame: {i}", 'Processed Frame', 'Bounding Boxes']
@@ -195,6 +243,19 @@ for file in non_calibrated_files:
 
             if key == 27:
                 break
+
+        average_x, average_y, average_width, average_height = calculate_weighted_average_bbox(total_x, total_y, total_w, total_h, total_weight)
+        reference_frame = get_reference_frame(thermal)
+        reference_frame_copy = cv2.resize(np.copy(reference_frame).astype('uint8'), (320, 320))
+        for i in range(len(average_x)):
+            cv2.rectangle(reference_frame, (average_x[i], average_y[i]), (average_x[i]+average_width[i], average_y[i]+average_height[i]), (255, 255, 255), 1)
+        reference_frame = cv2.resize(reference_frame.astype('uint8'), (320, 320))
+        images_to_show = [reference_frame_copy, reference_frame]
+        captions = ["Original Image", 'Final Bounding Box Prediction']
+        imgStack = stackImages(1, [images_to_show], [captions])
+        cv2.imshow("Final Prediction",imgStack)
+        cv2.waitKey(0)
+
 
 
 cv2.destroyAllWindows()
